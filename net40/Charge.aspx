1068e0c5fa82988227a66d6a304cf5cc9c0e6570
Gaurav Mishra <gauravmishra@cmp-244.mshome.net>
2020-11-09 23:49:00 +0530

    First commit

---
+++ net40/Charge.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Charge.aspx.cs" Inherits="RazorpaySampleApp.Charge" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="container">
+      
+                <div class="jumbotran">        
+                   <h1 runat="server" id="h1Message">  </h1>
+                    <p runat="server" id="pTxnId"> </p>
+                    <p runat="server" id="pOrderId"> </p>
+                 </div> 
+    </div>
+    </form>
+</body>
+</html>
---
+++ net40/Charge.aspx.cs
@@ -0,0 +1,46 @@
+﻿using Razorpay.Api;
+using System;
+using System.Collections.Generic;
+
+namespace RazorpaySampleApp
+{
+    public partial class Charge : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try { 
+                    string paymentId = Request.Form["razorpay_payment_id"];
+                    string orderId = Request.Form["razorpay_order_id"];
+                    string signature = Request.Form["razorpay_signature"];
+
+
+                    string key = "rzp_test_Ds7nLlw2c0Flgb";
+                    string secret = "tfjeAExzVHGoMQ9zDeM5Ae82";
+
+                    RazorpayClient client = new RazorpayClient(key, secret);
+
+                 
+                    Dictionary<string, string> attributes = new Dictionary<string, string>();
+               
+                    attributes.Add("razorpay_payment_id", paymentId);
+                    attributes.Add("razorpay_order_id", orderId);
+                    attributes.Add("razorpay_signature", signature);
+
+                    Utils.verifyPaymentSignature(attributes);
+
+                    pTxnId.InnerText = paymentId;
+                    pOrderId.InnerText = orderId;
+                    h1Message.InnerText = "Transaction Successful";
+        }
+        catch(Exception)
+        {
+           h1Message.InnerText = "Transaction UnSuccessful";
+        }
+
+//             please use the below code to refund the payment 
+//             Refund refund = new Razorpay.Api.Payment((string) paymentId).Refund();
+
+           // Console.WriteLine(paymentId);
+        }
+    }
+}
